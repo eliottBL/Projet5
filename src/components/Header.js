e4d12684b4cd07e609101fefab317536d6116611
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-03 15:47:58 +0200

    feat: création de ./components + App.js dans components + index.js redirigé + création de Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (<header>
+        <img>...</img>
+        <nav>
+
+        </nav>
+        <h1>Chez vous, partout et ailleurs</h1>
+    </header>)
+}
+
+export default Header
