6a1e1f9d73f8b409f095bed44530566704acad32
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-03 18:40:31 +0200

    feat(components):footer.css footer.js ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import '../styles/index.css';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
-  return  <Header />
+  return  (<div className="container">
+      <Header />
+      <Footer />
+  </div>)
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import logo from '../assets/logo-kasa-NB.png';
+import '../styles/Footer.css';
+
+function Footer () {
+return <footer>
+            <img src={logo} alt="logo kasa noir et blanc"></img>
+            <p>© 2020 Kasa. All rights reserved</p>
+       </footer>
+};
+
+export default Footer;
