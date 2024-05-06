ab03b4909077cc982a88e5b49b37309be1151b43
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-06 16:42:58 +0200

    feat: logement.json et Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../styles/index.css';
 import Header from './Header';
+import Main from './Main';
 import Footer from './Footer';
 
 function App() {
   return  (<div className="container">
       <Header />
+      <Main />
       <Footer />
   </div>)
 }
