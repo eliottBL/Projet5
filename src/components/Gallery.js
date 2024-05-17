384c66c7f262c4de2cdf4210487754e557a4a051
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-17 19:43:49 +0200

    fix: logements.json -> public folder

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,27 @@
 import '../styles/Gallery.scss'
-import listeLogements from './logements.json'
+//import listeLogements from '../../public/logements.json'
 import { useState, useEffect } from 'react'
 
 function Gallery() {
 
-    return (
-        <section className='gallery'>
-            {listeLogements.map((logement) => (
-                <div key={logement.id} className='card'>
-                    <h2>{logement.title}</h2>
-                </div>
-
-            ))}
-        </section>
-
-    )
+    useEffect(() => {
+        fetch("../../public/logements.json")
+            .then((response) => console.log(response.json())
+                .catch((error) => console.log(error))
+            )
+    }, [])
+    /*
+        return (
+            <section className='gallery'>
+                {listeLogements.map((logement) => (
+                    <div key={logement.id} className='card'>
+                        <h2>{logement.title}</h2>
+                    </div>
+    
+                ))}
+            </section>
+    
+        )*/
 }
 
 export default Gallery; 
