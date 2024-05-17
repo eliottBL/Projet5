e033eee4898df93a9caf458bac78a599f7d75339
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-17 20:17:31 +0200

    feat: création src>data>protoLogements.json

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,27 +1,32 @@
 import '../styles/Gallery.scss'
-//import listeLogements from '../../public/logements.json'
+import listeLogements from '../data/protoLogements.json'
 import { useState, useEffect } from 'react'
 
 function Gallery() {
-
-    useEffect(() => {
-        fetch("../../public/logements.json")
-            .then((response) => console.log(response.json())
-                .catch((error) => console.log(error))
-            )
-    }, [])
     /*
-        return (
-            <section className='gallery'>
-                {listeLogements.map((logement) => (
-                    <div key={logement.id} className='card'>
-                        <h2>{logement.title}</h2>
-                    </div>
-    
-                ))}
-            </section>
-    
-        )*/
+        useEffect(() => {
+            fetch("../../public/logements.json", {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+                .then((response) => response.json())
+                .then((response) => console.log(response))
+                .catch((error) => console.log(error))
+        }, [])
+        */
+    return (
+        <section className='gallery'>
+            {listeLogements.map((logement) => (
+                <div key={logement.id} className='card'>
+                    <h2>{logement.title}</h2>
+                </div>
+
+            ))}
+        </section>
+
+    )
 }
 
 export default Gallery; 
