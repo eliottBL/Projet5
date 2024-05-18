6f22bfbd6d7aa42530b5a78f1a2910b82d0372a7
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-18 09:07:57 +0200

    fix(pages>Apropos): import Collapses et import de Collapse dans Collapses

--- src/components/Collapses.js
+++ src/components/Collapses.js
@@ -1,32 +1,15 @@
 import '../styles/Gallery.scss'
 import listeLogements from '../data/protoLogements.json'
 import { useState, useEffect } from 'react'
+import Collapse from './Collapse';
 
-function Gallery() {
-    /*
-        useEffect(() => {
-            fetch("../../public/logements.json", {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            })
-                .then((response) => response.json())
-                .then((response) => console.log(response))
-                .catch((error) => console.log(error))
-        }, [])
-        */
+
+function Collapses() {
     return (
-        <section className='gallery'>
-            {listeLogements.map((logement) => (
-                <div key={logement.id} className='card'>
-                    <h2>{logement.title}</h2>
-                </div>
-
-            ))}
+        <section>
+            <Collapse />
         </section>
-
     )
 }
 
-export default Gallery; 
+export default Collapses; 
--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -3,12 +3,14 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import imageUrl from '../assets/image-apropos.png'
 import Banner from '../components/Banner';
+import Collapses from '../components/Collapses';
 
 function Apropos() {
   return (<div className="container">
     <Header />
     <main>
       <Banner imageUrl={imageUrl} />
+      <Collapses />
     </main>
     <Footer />
   </div>)
