1549c3ebf64e3f54d94a51d35032841cf226e0a4
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-18 14:02:15 +0200

    feat(styles): création Collapses.css et ajout className Collapse depuis Collapses

--- src/components/Collapses.js
+++ src/components/Collapses.js
@@ -1,4 +1,4 @@
-import '../styles/Gallery.scss'
+import '../styles/Collapses.scss'
 import listeLogements from '../data/protoLogements.json'
 import { useState, useEffect } from 'react'
 import Collapse from './Collapse';
@@ -6,11 +6,11 @@ import Collapse from './Collapse';
 
 function Collapses() {
     return (
-        <section>
-            <Collapse title={'Fiabilités'} contenue={'Les annonces postées sur Kasa garantissent une fiabilité[...]par nos équipes.'} />
-            <Collapse title={'Respect'} contenue={'La bienveillance fait partie des valeurs[...]de notre plateforme.'} />
-            <Collapse title={'Service'} contenue={'La bienveillance fait partie des valeurs[...]de notre plateforme.'} />
-            <Collapse title={'Sécurité'} contenue={'La sécurité est la priorité[...]domestique pour nos hôtes.'} />
+        <section className='collapse-container'>
+            <Collapse className='collaps' title={'Fiabilités'} contenue={'Les annonces postées sur Kasa garantissent une fiabilité[...]par nos équipes.'} />
+            <Collapse className='collaps' title={'Respect'} contenue={'La bienveillance fait partie des valeurs[...]de notre plateforme.'} />
+            <Collapse className='collaps' title={'Service'} contenue={'La bienveillance fait partie des valeurs[...]de notre plateforme.'} />
+            <Collapse className='collaps' title={'Sécurité'} contenue={'La sécurité est la priorité[...]domestique pour nos hôtes.'} />
         </section>
     )
 }
