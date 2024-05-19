49c9e56b623fca6e1aeb1bdfb074f60fd58c4917
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-19 10:45:08 +0200

    feat(pages): création page logement.jsx

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,22 +1,11 @@
 import '../styles/index.scss';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import imageUrl from '../assets/image-apropos.png'
-import Banner from '../components/Banner';
-import Collapses from '../components/Collapses';
 
-function Apropos() {
+
+function Logement() {
   const texte = ''
-  return (<div className="container">
-    <Header />
-    <main>
-      <Banner texte={texte} imageUrl={imageUrl} />
-      <section>
-        <Collapses />
-      </section>
-    </main>
-    <Footer />
-  </div>)
+  return (
+    <p>{id}</p>
+  )
 }
 
-export default Apropos;
+export default Logement;
