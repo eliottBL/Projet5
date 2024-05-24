a91436e3a463c2fa53064f7888c5fc9d5f133b0e
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-24 20:35:11 +0200

    fix(Carroussel.js): Carroussel fonctionnel infinis + creéation de Caroussel.scss

--- src/components/Carroussel.js
+++ src/components/Carroussel.js
@@ -1,24 +1,36 @@
+import svg from '../assets/fleche.png'
+import '../styles/Carroussel.scss'
+import { useState, useEffect } from 'react'
+
 function Carroussel({ pictures }) {
 
-    const picturesArray = { pictures }
-    let i = 0
-    let src = picturesArray[i]
+    const picturesArray = { pictures }.pictures
+    const [index, setIndex] = useState(0)
     const Prev = () => {
-        i - 1
-        if (i < 0) i = picturesArray.length
-    }
-    const Next = () => {
-        i + 1
-        if (i > picturesArray.length) i = 0
+        let newIndex = index - 1
+        if (newIndex < 0) {
+            setIndex(picturesArray.length - 1)
+        }
+        else { setIndex(index - 1) }
     }
 
-    <div className="carroussel">
-        <div>
-            <img className="prev" onClick={Prev()}></img>
-            <img className="next" onClick={Next()}></img>
-        </div>
-        <img className="carroussel-gallery" src={src}></img>
-    </div>
+    const Next = () => {
+        let newIndex = index + 1
+        if (newIndex > picturesArray.length - 1) {
+            setIndex(0)
+        }
+        else { setIndex(index + 1) }
+    }
+
+
+    return (
+        <div className="carroussel">
+            <div>
+                <img className="prev" onClick={Prev} src={svg}></img>
+                <img className="next" onClick={Next} src={svg}></img>
+            </div>
+            <img className="carroussel-gallery" src={picturesArray[index]}></img>
+        </div>)
 }
 
 export default Carroussel;
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,15 +1,20 @@
+import Carroussel from '../components/Carroussel';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../styles/index.scss';
 import { useParams } from 'react-router';
+import listeLogements from '../data/protoLogements.json'
 
 function Logement() {
   const { id } = useParams()
+  const logement = listeLogements.filter(element => element.id == id)
+  const pictures = logement[0].pictures
   return (
     <div>
       <Header />
       <main>
         <p>{id}</p>
+        <Carroussel pictures={pictures} />
       </main>
       <Footer />
     </div>
