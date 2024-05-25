ff4fb927562d62882cce39d4ed2decf7f6896bc3
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-25 10:31:51 +0200

    feat(component):InfoLogement.js créer, pour la section sous le caroussel

---
+++ src/components/InfoLogement.js
@@ -0,0 +1,29 @@
+function InfoLogement() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h2 className="title"></h2>
+                    <h3 className="location"></h3>
+                </div>
+                <div>
+                    <div>
+                        <p className="nom"></p>
+                        <p className="prenom"></p>
+                    </div>
+                    <img></img>
+                </div>
+            </div>
+            <div>
+                <div className="tag">
+
+                </div>
+                <div className="rate">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default InfoLogement;
