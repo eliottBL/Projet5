6060655965ffcc1b7ff9ddf4859b06c07a7b5a2f
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-27 14:05:00 +0200

    fix: rating fonctionnel, retourne nb étoile selon valeurs rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,12 +1,28 @@
+import svg from '../assets/star.svg'
+import '../styles/Rating.scss'
 
-function Rating() {
-    const rate = Number
+function Rating({ rating }) {
+    const rate = rating
+    console.log(rate)
+    let note = []
+
     for (let i = 0; i < rate; i++) {
-        return (<img className="red"></img>)
+        note.push('red')
     }
     for (let i = 0; i < 5 - rate; i++) {
-        return (<img className="grey"></img>)
+        note.push('grey')
     }
+    return (
+        <div className='rate'>
+            {note.map((classe, index) => (
+                <div key={`${classe}-${index}`}>
+                    <img className={classe} src={svg}></img>
+                </div>
+            ))}
+        </div>
+    )
+
 }
+
 
 export default Rating;
