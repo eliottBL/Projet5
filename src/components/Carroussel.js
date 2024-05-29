05dc149693660d2220b57d9f4e25c562b31b4d6d
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-05-29 08:50:38 +0200

    fix(carroussel.js & rating.js): code simplifié fonctionnel

--- src/components/Carroussel.js
+++ src/components/Carroussel.js
@@ -1,33 +1,31 @@
 import svg from '../assets/fleche.png'
 import '../styles/Carroussel.scss'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function Carroussel({ pictures }) {
 
     const picturesArray = { pictures }.pictures
     const [index, setIndex] = useState(0)
-    const Prev = () => {
-        let newIndex = index - 1
-        if (newIndex < 0) {
+    const prev = () => {
+        if (index - 1 < 0) {
             setIndex(picturesArray.length - 1)
         }
-        else { setIndex(index - 1) }
+        else setIndex(index - 1)
     }
 
-    const Next = () => {
-        let newIndex = index + 1
-        if (newIndex > picturesArray.length - 1) {
+    const next = () => {
+        if (index + 1 > picturesArray.length - 1) {
             setIndex(0)
         }
-        else { setIndex(index + 1) }
+        else setIndex(index + 1)
     }
 
 
     return (
         <div className="carroussel">
             <div>
-                <img className="prev" onClick={Prev} src={svg}></img>
-                <img className="next" onClick={Next} src={svg}></img>
+                <img className="prev" onClick={prev} src={svg}></img>
+                <img className="next" onClick={next} src={svg}></img>
             </div>
             <img className="carroussel-gallery" src={picturesArray[index]}></img>
         </div>)
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,21 +2,13 @@ import svg from '../assets/star.svg'
 import '../styles/Rating.scss'
 
 function Rating({ rating }) {
-    const rate = rating
-    console.log(rate)
-    let note = []
+    let note = [1, 2, 3, 4, 5]
 
-    for (let i = 0; i < rate; i++) {
-        note.push('red')
-    }
-    for (let i = 0; i < 5 - rate; i++) {
-        note.push('grey')
-    }
     return (
         <div className='rate'>
-            {note.map((classe, index) => (
-                <div key={`${classe}-${index}`}>
-                    <img className={classe} src={svg}></img>
+            {note.map((index) => (
+                <div key={index}>
+                    <img className={`${index < rating ? 'red' : 'grey'}`} src={svg}></img>
                 </div>
             ))}
         </div>
