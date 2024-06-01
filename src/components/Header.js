cd576390202ca1d1fd511a1247e2cc43e1ec6b19
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-06-01 10:27:13 +0200

    fix(header): underline quand lien actif

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import logo from '../assets/logo-kasa.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Header.scss';
 
 function Header() {
+    let AccStyle = 'none'
+    let AppStyle = 'none'
+    const page = useLocation()
+
+    if (page.pathname == '/') {
+        AccStyle = 'underline'
+        AppStyle = 'none'
+    } else if (page.pathname == '/apropos') {
+        AccStyle = 'none'
+        AppStyle = 'underline'
+    }
+
+
+
     return (<header>
         <img src={logo} alt='logo Kasa' className="logo" />
         <nav className="menu">
-            <Link to="/">Accueil</Link>
-            <Link to="/apropos">A Propos</Link>
+            <Link className={AccStyle} to="/">Accueil</Link>
+            <Link className={AppStyle} to="/apropos">A Propos</Link>
         </nav>
     </header>)
 }
