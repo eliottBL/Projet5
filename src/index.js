c5fc182d4300812f6f5ba1805975baef3353afaa
eliottblaiselassire <eliott.blaise1304@gmail.com>
2024-06-13 12:21:20 +0200

    fix:style gallery et banner titre

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Logement from './pages/Logement';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
     <Router>
       <Routes>
         <Route path="/" element={<Accueil />} />
